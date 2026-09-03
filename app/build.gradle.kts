e08f158cfa8a262cbb3a15dfc4446add817aeea3
GAIDI AMJED <150559809+Gautier7799@users.noreply.github.com>
2026-09-03 12:35:23 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,38 @@
-defaultConfig {
-    applicationId = "com.aistudio.newapp.xkdjqm"
-    minSdk = 33
-    targetSdk = 34
-    versionCode = 1
-    versionName = "1.0"
-  }
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.mywatchfaces"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.mywatchfaces"
+        minSdk = 30
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    androidResources {
+        noCompress.add("xml")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("com.google.android.gms:play-services-wearable:18.1.0")
+}
